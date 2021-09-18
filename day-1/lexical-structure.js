3ae6eda243cfeb7dc3a75b7faff4d012a3cadf05
Enes Ozturk <enesozturk.d@gmail.com>
2021-09-18 12:35:50 +0300

    first lesson: lexical structure

---
+++ day-1/lexical-structure.js
@@ -0,0 +1,55 @@
+// Comment Line
+
+/*
+Multi line 
+comment line
+*/
+
+// Data Types
+
+// Primitive Type
+// Numbers --> float
+// Strings --> "Kodluyoruz", 'Kodluyoruz'
+// Booleans
+// null
+// undefined
+
+// console.log(123);
+// console.log("Kodluyoruz");
+// console.log(true, false);
+// console.log(null);
+// console.log(undefined);
+
+// Reserved Words
+// var, void, for, typeof, function...
+
+// var, ES6 -> let, const
+// var a = 123;
+// var b = "Kodluyoruz";
+// var c = true;
+// var d = null;
+// var e = undefined;
+
+// Type safe
+
+// = operator
+let a = 123;
+a = "Kodluyoruz";
+a = 33333;
+// console.log(typeof a);
+
+// Mutable & Immutable
+// Mutable -> Değiştirilebilir
+// Immutable -> Değiştirilemez
+
+const b = "Kodluyoruz";
+
+// ------------
+
+var a1 = 123;
+a1 = 12345455;
+a1 = "Enes";
+
+var a2 = 123;
+
+console.log(a2);
